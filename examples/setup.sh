a6643b55af7d0dd3a070feb771a6caa42bb73bcf
Makis Tracend <makis.tracend@gmail.com>
2014-11-21 17:13:51 -0800

    scaffolding

---
+++ examples/setup.sh
@@ -0,0 +1,15 @@
+
+# Installing the CRUDr CLI
+
+# 1. Install the main executable
+npm install -g crudr-cli
+
+#2. Setup onscribe (only needed once)
+crudr setup
+
+# you'll be propted with two entries
+# - email: ...
+# - token: ...
+
+# 3. Find out API commands using the help
+crudr -h
